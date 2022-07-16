2550909a686f0880a862e8d3827d800d8d277108
Huggo Parcelly <h.parcelly@gmail.com>
2022-07-16 16:27:13 -0300

    add documentacao do PersonService

--- Service/PersonService.hs
+++ Service/PersonService.hs
@@ -5,7 +5,11 @@ module Service.PersonService where
   import Util.IdManager
   import Util.StringManager
   
-  
+  -- Cria uma pessoa que podera ser um doador ou um receptor
+  -- pede ao usuario que digite os dados da pessoa a ser criada
+  -- cria uma entidade pessoa e adiciona esse conteudo em um txt
+  -- imprime na tela que o usuario foi cadastrado com sucesso
+  -- Parâmetros= fileName: o nome do aquivo onde será buscado o id
   createPerson :: String -> IO()
   createPerson fileName = do
     putStr "Nome: "
@@ -25,14 +29,28 @@ module Service.PersonService where
     putStrLn "Usuário cadastrado com sucesso!"
 
 
+  -- Realiza a busca de todas as pessoas nos bancos de dados de doador ou receptores
+  -- Parâmetros= fileName: o nome do aquivo onde será buscado o id
+  -- Retorna um array com todas as pessoas do banco escolhido
   getAllPeople :: String -> IO [String]
   getAllPeople = readContent
 
+  -- Busca uma pessoa pelo cpf
+  -- é solicitado que o usuario digite o cpf a ser buscado
+  -- Parâmetros= fileName: o nome do aquivo onde será buscado o id
+  -- Retorna uma string com a pessoa busca ou com o valor "não encontrado"
   getByCpf :: String -> IO String
   getByCpf fileName = do
     cpfToFind <- searchCpf
     getByContent fileName cpfToFind
 
+  -- Atualiza uma pessoa
+  -- busca a pessoa que deseja atualizar pelo cpf
+  -- pede ao usuario que digite os dados atualizados da pessoa
+  -- cria uma entidade pessoa com novos dados 
+  -- atualiza uma pessoa com a funcao updateByContent
+  -- imprime na tela que o usuario foi cadastrado com sucesso
+  -- Parâmetros= fileName: o nome do aquivo onde será buscado o id
   putByCpf :: String -> IO ()
   putByCpf fileName = do
     cpfToFind <- searchCpf
@@ -50,11 +68,14 @@ module Service.PersonService where
 
     let newPessoa = Person cpf name tel end tipoSangue
 
-    deleteByContent fileName cpfToFind
-    addContent fileName $ show newPessoa
+    updateByContent fileName cpfToFind $ show newPessoa
     
     putStrLn "Usuário atualizado com sucesso."
 
+  -- Funcao que deleta uma pessoa do seu respectivo banco de dados
+  -- solicita que o usuario digite o cpf a ser deletado
+  -- realiza a delecao com a funcao deleteByContent
+  -- Parâmetros= fileName: o nome do aquivo onde será buscado o id
   deletePerson :: String -> IO ()
   deletePerson fileName = do
     cpfToFind <- searchCpf
