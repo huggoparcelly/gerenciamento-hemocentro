073339a82a3213ad757869dbc3e13f38c0d0f935
Crisley Venâncio Marques <crisley.marques@ccc.ufcg.edu.br>
2022-08-05 15:33:06 -0300

    Divide os managers em arquivos diferentes.

---
+++ prolog/src/Service/DoacaoService.pl
@@ -0,0 +1,20 @@
+
+
+ehDoador(Cpf) :- ce
+
+createDoacao(Cpf, TipoSangue, Qtd) :-
+    ehDoador()
+    % pega a data atual
+    % instancia uma nova doacao 
+    % pega as bolsas atuais desse tipo sanguineo
+    % atualiza a quantidade
+    % update a o database de bolsas 
+    % adiciona a doacao no seu database
+
+% getQtdFromBolsas
+
+% createDoacaoDirecionada
+
+% getAllDoacoes
+
+% getDoacaoById
