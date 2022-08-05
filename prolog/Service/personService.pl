76b11d00a95dcbda27fbbb7f10e882ed9ae12bf8
Mariane Zeitouni <marianezei@gmail.com>
2022-08-05 15:36:43 -0300

    personService

---
+++ prolog/Service/personService.pl
@@ -0,0 +1,29 @@
+:-module(personService, [getUserByCpf/3, getAllUsers/1, addUser/6, removeUser/2, updateUser/6]).
+personService:-use_module('../Util/jsonManager.pl', 
+    [getPersonByID/3, 
+    getAllPersons/1, 
+    addPerson/6, 
+    removerPerson/2, 
+    checaExistencia/2, 
+    updatePerson/6]).
+
+addUser(FileName, Cpf, Nome, Tel, Endereco, TipoSangue):-
+    writeln('Insira os dado do usuario: '),
+    addPerson(FileName, Cpf, Nome, Tel, Endereco, TipoSangue).
+
+removeUser(FileName, Cpf):-
+    removerPerson(FileName, Cpf),
+    writeln('Usuário removido com sucesso!').
+
+getUserByCpf(FileName, Cpf, Person):-
+    writeln('Usuário procurado: '),
+    getPersonByID(FileName, Cpf, Person).
+    
+getAllUsers(FileName):-
+    writeln('Lista dos usuários: '),
+    getAllPersons(FileName).
+
+updateUser(FileName, Cpf, NovoNome, NovoTel, NovoEndereco, NovoTipoSangue):-
+    writeln('Digite os novos dados do usuário: '),
+    updatePerson(FileName, Cpf, NovoNome, NovoTel, NovoEndereco, NovoTipoSangue),
+    writeln('Usuário atualizado com sucesso!').
