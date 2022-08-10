d3cb59f452964a27179270b203bdcb6555cdd3ab
Crisley Venâncio Marques <crisley.marques@ccc.ufcg.edu.br>
2022-08-10 18:46:46 -0300

    Adiciona menu em loop.

--- prolog/src/menu.pl
+++ prolog/src/menu.pl
@@ -8,7 +8,7 @@
 
 clear :- writeln('\e[H\e[2J').
 
-menuPrincipal :- clear, writeln('\n\nBem vindo ao Gerenciamento de Hemocentro! \n'), subMenu.
+menuPrincipal :- writeln('\n\nBem vindo ao Gerenciamento de Hemocentro! \n'), subMenu.
 
 subMenu :-
     writeln('--------------------------------'),
@@ -96,33 +96,49 @@ menuInput('5') :-
     menuListar(Input).
 
 menuInput('6'):- halt.
+menuInput(_) :- opcaoInvalida, menuPrincipal.
 
-menuCadastro('1'):- addUser('doadores').
-menuCadastro('2'):- addUser('receptores').
-menuCadastro('3'):- createDonation.
+
+menuCadastro('1'):- addUser('doadores'), subMenu.
+menuCadastro('2'):- addUser('receptores'), subMenu.
+menuCadastro('3'):- createDonation, subMenu.
 % menuCadastro('4'):- createDirectDonation.
 % %menuCadastro('5'):- 
 % %menuCadastro('6'):- 
 menuCadastro('7'):- subMenu.
+menuCadastro(_) :- opcaoInvalida, menuInput('1').
 
-menuEdit('1'):- updateUser('doadores').
-menuEdit('2'):- updateUser('receptores').
+
+menuEdit('1'):- updateUser('doadores'), subMenu.
+menuEdit('2'):- updateUser('receptores'), subMenu.
 menuEdit('3'):- subMenu.
+menuEdit(_) :- opcaoInvalida, menuInput('2').
 
-menuBuscar('1'):- getUserByCpf('doadores').
-menuBuscar('2'):- getUserByCpf('receptores').
-menuBuscar('3'):- getDonationById.
+
+menuBuscar('1'):- getUserByCpf('doadores'), subMenu.
+menuBuscar('2'):- getUserByCpf('receptores'), subMenu.
+menuBuscar('3'):- getDonationById, subMenu.
 %menuBuscar('4'):- 
 menuBuscar('5'):- subMenu.
+menuBuscar(_) :- opcaoInvalida, menuInput('3').
 
-menuRemover('1'):- removeUser('doadores').
-menuRemover('2'):- removeUser('receptores').
+
+menuRemover('1'):- removeUser('doadores'), subMenu.
+menuRemover('2'):- removeUser('receptores'), subMenu.
 menuRemover('3'):- subMenu.
+menuRemover(_) :- opcaoInvalida, menuInput('4').
 
-menuListar('1'):- getAllUsers('doadores').
-menuListar('2'):- getAllUsers('receptores').
-menuListar('3'):- getAllDonations.
+
+menuListar('1'):- getAllUsers('doadores'), subMenu.
+menuListar('2'):- getAllUsers('receptores'), subMenu.
+menuListar('3'):- getAllDonations, subMenu.
 %menuListar('4'):- 
 %menuListar('5'):- 
 %menuListar('6'):- 
 menuListar('7'):- subMenu.
+menuListar(_) :- opcaoInvalida, menuInput('5').
+
+
+opcaoInvalida :- 
+    clear,
+    writeln('Opção inválida. Tente novamente!').
